754fa87790c807496905bb9126c98340f128c818
sufyanAbbasi <sufy.abbasi@gmail.com>
2017-06-06 11:56:40 -0700

    Primitive UI for reporting smells

--- summaryDialog.js
+++ summaryDialog.js
@@ -4,9 +4,20 @@ function openSummaryDialog() {
   $("#summaryDialog").dialog("open");
 }
 
+function openReportDialog(){
+	$("#reportDialog").dialog("open");
+}
+
 $(function() {
   $("#summaryDialog").dialog({
     autoOpen: false,
     width: "50%"
+  });
+});
+
+$(function() {
+  $("#reportDialog").dialog({
+    autoOpen: false,
+    width: "60%"
   });
 });
