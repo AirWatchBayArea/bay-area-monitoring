92bd82b23a5cc60162e528c39560c037fdbb4569
sufyanAbbasi <sufy.abbasi@gmail.com>
2017-06-07 06:40:58 -0700

    Implemented backend serialization of reporting form, geocoding, and post to server

---
+++ report.js
@@ -0,0 +1,88 @@
+var serverURL = 'http://bayarea.staging.api.smellpittsburgh.org/api/v1/smell_reports';
+
+
+function openReportDialog(){
+	$("#reportDialog").dialog("open");
+}
+
+$(function() {
+  $("#reportDialog").dialog({
+    autoOpen: false,
+    width: "60%"
+  });
+});
+
+function makeid(length){
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for( var i=0; i < length; i++ )
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text;
+}
+
+function serializeForm(geocodeResults){
+	//userhash
+	if(!localStorage.getItem('AWBAuser')) {
+  		localStorage.setItem('AWBAuser', makeid(10));
+	}
+	//latlong
+	var latlng = geocodeResults[0]['geometry']['location'];
+	var data = JSON.stringify(
+	{
+		"user_hash" : MD5(localStorage.getItem('AWBAuser')),
+	    "latitude" : latlng.lat(),
+	    "longitude" : latlng.lng(),
+	    "smell_value" : parseInt($('input[name=smell]:checked').val()),
+	    "smell_description" : $('input[name=describe-air]').val(),
+	    "feelings_symptoms" : $('input[name=symptoms]').val(),
+	    "submit_achd_form" : false
+	});
+	postData(data);
+}
+
+function postData(data){
+	$.ajax({
+	  method: 'POST',
+	  url: serverURL,
+	  data: data,
+	}).done(function(msg) {
+	  console.log('huzzah!')
+	});
+}
+
+function initGeocode(){
+	var geocoder = new google.maps.Geocoder();
+	$('#report-form').submit(function(event){
+		 event.preventDefault();
+		 geocodeAddress(geocoder, serializeForm);
+	});
+}
+
+function geocodeAddress(geocoder, callback) {
+	var address = document.getElementById('address').value;
+	geocoder.geocode({'address': address, 'bounds': 
+		new google.maps.LatLngBounds(
+			new google.maps.LatLng(37.851624286540286, -122.56790076098628), 
+			new google.maps.LatLng(38.14975803797967,-121.97875891528315))
+	 }, function(results, status) {
+	  if (status === 'OK') {
+	    callback(results);
+	  } else if (status === 'ZERO_RESULTS'){
+	    reportFailed('address to coordinate conversion failed.\nPlease be more exact in your location description.');
+	  }else{
+	  	reportFailed('of internal error. The error will be reported.');
+	  	//REPORT ERROR HERE
+	  }
+	});
+}
+
+function reportFailed(reason){
+	alert('Smell report failed because ' + reason);
+}
+
+$(initGeocode);
+
+
+
--- summaryDialog.js
+++ summaryDialog.js
@@ -4,20 +4,10 @@ function openSummaryDialog() {
   $("#summaryDialog").dialog("open");
 }
 
-function openReportDialog(){
-	$("#reportDialog").dialog("open");
-}
 
 $(function() {
   $("#summaryDialog").dialog({
     autoOpen: false,
     width: "50%"
-  });
-});
-
-$(function() {
-  $("#reportDialog").dialog({
-    autoOpen: false,
-    width: "60%"
   });
 });
