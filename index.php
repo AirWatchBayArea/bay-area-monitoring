2d4861657bc3abf7604be5962927eb95cb65986a
sufyanAbbasi <sufy.abbasi@gmail.com>
2017-08-06 23:07:29 -0400

    Staging message

--- index.php
+++ index.php
@@ -321,6 +321,7 @@
   <div id="report-pollution-page" class="resource-container">
     <h2>Report a Pollution Incident<br><span style="font-size: 25px; color: gray; font-weight: 500">(i.e. flaring, odor, residue, health symptoms)</span></h2>
     <p class="note">When you report to Air Watch Bay Area, you contribute to a <a href="#user-reports" class="underline">publicly visible “paper&nbsp;trail” of incidents.</a><br>Having this paper trail enables community members to hold Bay Area Air Quality Management District accountable.</p>
+    <p class="note error">THIS IS THE STAGING SITE! Reports without photos will go to a fake server so feel free to test away. Pictures will be uploaded, however, but email <a href="mailto:sufy.abbasi@gmail.com">sufy.abbasi@gmail.com</a> and we will remove it for you!</p>
 
     <form id="report-form">
       <div class="report-form-section">
