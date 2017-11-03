0df6111432a047300c22c0ee7268f63081f78b83
sufyanAbbasi <sufy.abbasi@gmail.com>
2017-11-03 02:44:04 -0400

    added AWBA email and moved FB widget

--- index.php
+++ index.php
@@ -202,6 +202,7 @@
             <div class="intro-text" style="width:76%; margin: 0 12%">
               <img style="margin-bottom: 5%" src="img/logo+text.svg" class="img-reponsive"/>
                 <div style="text-shadow: 0px 0px 20px #162946" class="intro-lead-in" lang="en">An interactive tool for the frontline communities of the San Francisco Bay Area to explore our air quality.</div>
+                <a href="https://www.facebook.com/AirWatchBayArea/?ref=br_rs" target="_blank" class="btn btn-xl text-gettingstarted" style="background-color: rgb(66, 103, 178); color: white; border: none;padding:15px 25px">Join our Facebook page &rarr;</a><br>
                 <a onclick="jumpToGetStarted()" class="page-scroll btn btn-xl" style="border-color: #f7b733; background-color: #f7b733; color: white;padding:15px 25px; letter-spacing: 1px;">GET STARTED</a>
             </div>
         </div>
@@ -248,7 +249,6 @@
                     </span>
                     <h4 class="service-heading">Resources for Action</h4>
                     <a href="#resources-for-action" class="btn btn-xl text-gettingstarted">View resources for action &rarr;</a><br>
-                    <a href="https://www.facebook.com/AirWatchBayArea/?ref=br_rs" target="_blank" class="btn btn-xl text-gettingstarted" style="background-color: rgb(66, 103, 178); color: white; border: none;padding:10px 20px">Like us on our Facebook page &rarr;</a>
                 </div>
             </div>
         </div>
@@ -309,7 +309,7 @@
             </div>
             <div class="row">
                 <div class="col-lg-7 col-sm-6">         
-                    <p class="service-heading">Air Watch is developed and maintained by the <a style="color: #FD9453;" href="https://www.fairtechcollective.org/">Fair Tech Collective</a> at Drexel University in collaboration with the <a style="color: #FD9453;" href="http://cmucreatelab.org/">Community Robotics, Education, and Technology Empowerment Lab</a> at Carnegie Mellon University and concerned community members from:</p>
+                    <p class="service-heading">Air Watch is developed and maintained by the <a style="color: #FD9453;" href="https://www.fairtechcollective.org/">Fair Tech Collective</a> (contact: <a href="mailto:airwatchbayarea@gmail.com">airwatchbayarea@gmail.com</a>) at Drexel University in collaboration with the <a style="color: #FD9453;" href="http://cmucreatelab.org/">Community Robotics, Education, and Technology Empowerment Lab</a> at Carnegie Mellon University and concerned community members from:</p>
           <ul style="list-style-type: none; color:#FD9453;line-height:30px; padding: 0;">
             <li><a style="margin-bottom: 0; font-size: .95em" href="http://www.sustainablebenicia.org/about" class="btn btn-xl text-gettingstarted">Benicia Good Neighbor Steering Committee →</a></li>
             <li><a style="margin-bottom: 0; font-size: .95em" href="https://crockett-rodeo-united.com/" class="btn btn-xl text-gettingstarted">Crockett-Rodeo United to Defend the Environment →</a></li>
@@ -328,10 +328,15 @@
     <footer>
         <div class="container">
             <div class="row">
-                <div class="col-md-6">
+                <div class="col-md-4">
                     <span class="copyright">Copyright &copy; Air Watch Bay Area 2017</span>
                 </div>
-                <div class="col-md-6">
+                <div class="col-md-4">
+                    <ul class="list-inline quicklinks">
+                        <li><a href="mailto:airwatchbayarea@gmail.com" style="color:#f7b733">Contact: airwatchbayarea@gmail.com</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-4">
                     <ul class="list-inline quicklinks">
                         <li><a class="page-scroll" href="#page-top" style="color:#f7b733">Back to Top</a>
                         </li>
