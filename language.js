0183b1d5099fdbe1d9c7183c7dbb6ef9d795d11d
sufyanAbbasi <sufy.abbasi@gmail.com>
2018-06-19 18:30:53 -0700

    Added auomatic language detection

--- language.js
+++ language.js
@@ -1,5 +1,10 @@
+var supportedLanguages = ["en", "es"];
+//includes polyfill
+Array.prototype.includes||Object.defineProperty(Array.prototype,"includes",{value:function(r,e){if(null==this)throw new TypeError('"this" is null or not defined');var t=Object(this),n=t.length>>>0;if(0===n)return!1;var i,o,a=0|e,u=Math.max(a>=0?a:n-Math.abs(a),0);for(;u<n;){if((i=t[u])===(o=r)||"number"==typeof i&&"number"==typeof o&&isNaN(i)&&isNaN(o))return!0;u++}return!1}});
+
 function localizeInit(){
-	localize();
+	var userLang = window.navigator.userLanguage || window.navigator.language || 'en'; 
+	localize(supportedLanguages.includes(userLang) ? userLang : 'en');
 	$('.language').click(function(event){
 		var lang = event.target.dataset.lang;
 	    localize(lang);
